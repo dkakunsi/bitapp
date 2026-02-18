9efa8e578221cad06f7a913788f00d97863c16b6
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2026-02-18 11:00:31 +0000

    Use Java 21 for gradle.

--- mobile/app/android/app/build.gradle.kts
+++ mobile/app/android/app/build.gradle.kts
@@ -22,12 +22,12 @@ android {
     namespace = "com.cortech.bitapp"
     compileSdk = flutter.compileSdkVersion
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
 
     defaultConfig {
