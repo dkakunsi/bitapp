36c38af831022006404c4e47a9cef1293dd76978
Deddy Kakunsi <deddy.kakunsi@gmail.com>
2026-06-13 15:58:39 +0000

    Update android versioning.

--- mobile/app/android/app/build.gradle.kts
+++ mobile/app/android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 
     defaultConfig {
         applicationId = "io.dkakunsi.bitapp"
-        minSdk = 34
+        minSdk = 21
         targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
